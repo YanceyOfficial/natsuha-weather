6d208087317fc1b777283c93baa4e837c06fcc34
yanceyleo <developer@yanceyleo.com>
2019-02-17 22:33:40 +0800

    delete useless files

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -67,24 +67,6 @@ class Index extends Component {
     counterStore.incrementAsync();
   };
 
-  getSystemInfo = () => {
-    const res = Taro.getSystemInfoSync();
-    console.log(res.brand);
-    console.log(res.model);
-    console.log(res.system); // 可拿到系统版本 iOS 12.1.4
-    console.log(res.screenWidth);
-    console.log(res.screenHeight);
-    console.log(res.pixelRatio);
-    console.log(res.windowWidth);
-    console.log(res.windowHeight);
-    console.log(res.version); // 微信版本 7.0.3
-    console.log(res.statusBarHeight);
-    console.log(res.platform); // 可用于判断安卓还是iOS ios
-    console.log(res.language); // 判断语言 zh-CN ja 注：这里的语言跟系统语言设置无关，因为微信在设置里可以自定义语言
-    console.log(res.fontSizeSetting);
-    console.log(res.SDKVersion);
-  };
-
   render() {
     const {
       counterStore: { counter }
@@ -95,7 +77,6 @@ class Index extends Component {
         <Button onClick={this.decrement}>-</Button>
         <Button onClick={this.incrementAsync}>Add Async</Button>
         <Text>{counter}</Text>
-        <Button onClick={this.getSystemInfo}>getSystemInfo</Button>
       </View>
     );
   }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -15,3 +15,21 @@ export const setLoadingToast = (title: string = 'Loading...', mask: boolean = fa
     mask,
   })
 };
+
+export const getSystemInfo = () => {
+  const res = Taro.getSystemInfoSync();
+  console.log(res.brand);
+  console.log(res.model);
+  console.log(res.system); // 可拿到系统版本 iOS 12.1.4
+  console.log(res.screenWidth);
+  console.log(res.screenHeight);
+  console.log(res.pixelRatio);
+  console.log(res.windowWidth);
+  console.log(res.windowHeight);
+  console.log(res.version); // 微信版本 7.0.3
+  console.log(res.statusBarHeight);
+  console.log(res.platform); // 可用于判断安卓还是iOS ios
+  console.log(res.language); // 判断语言 zh-CN ja 注：这里的语言跟系统语言设置无关，因为微信在设置里可以自定义语言
+  console.log(res.fontSizeSetting);
+  console.log(res.SDKVersion);
+};
