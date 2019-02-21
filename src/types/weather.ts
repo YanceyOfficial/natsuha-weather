e6d5b2099a1125204b8df1e9f94db86d80d3265d
yanceyleo <developer@yanceyleo.com>
2019-02-21 13:31:37 +0800

    support get data by cur location

---
+++ src/types/weather.ts
@@ -0,0 +1,111 @@
+export interface IMeta {
+  conditionMap: {};
+  skycode: {};
+}
+
+export interface IWeather {
+  woeid?: number;
+  unit?: string;
+  sunAndMoon: ISunAndMoon;
+  provider: IProvider;
+  precipitations: IPrecipitation[];
+  photos: IPhoto[];
+  observation: IObservation;
+  location: ILocation;
+  forecasts: IForecasts;
+}
+
+interface ISunAndMoon {
+  moonPhase: number;
+  sunrise: number;
+  sunset: number;
+}
+
+interface IProvider {
+  name: string;
+}
+
+interface IPrecipitation {
+  probability: number;
+  timeSlot: string;
+}
+
+interface IPhoto {
+  dayOrNight?: string;
+  id?: string;
+  owner?: string;
+  ownerName: string;
+  resolutions: IResolution[];
+}
+
+interface IResolution {
+  height: number;
+  width: number;
+  url: string;
+}
+
+interface IObservation extends ICondition, IWind, ITemperature {
+  barometricPressure: number;
+  uvDescription: string;
+  uvIndex: number;
+  visibility: number;
+}
+
+interface IDayPartText {
+  text: string;
+  dayPart: string;
+}
+
+interface ILocation {
+  countryName: string;
+  displayName: string;
+  latitude?: number;
+  longitude?: number;
+  offsetSecs?: number;
+  photoWoeid?: number;
+  woeid?: number;
+}
+
+interface IForecasts {
+  daily: IDaily[];
+  hourly: IHourly[];
+}
+
+interface IDaily extends ICondition, ITemperature {}
+
+interface IHourly extends ICondition, IWind, ITemperature {}
+
+interface ICondition {
+  conditionCode: number;
+  conditionDescription: string;
+  dayPartTexts: IDayPartText[];
+  humidity: number;
+  localTime: {
+    day?: number;
+    hour?: number;
+    timestamp: Date;
+    weekday?: number;
+  };
+  observationTime: {
+    day: number;
+    hour: number;
+    timestamp: string;
+    weekday: number;
+  };
+  precipitationProbability: number;
+}
+
+interface IWind {
+  windDirection: number;
+  windDirectionCode: string;
+  windSpeed: number;
+}
+
+interface ITemperature {
+  temperature: {
+    feelsLike ? : number;
+    high ? : number;
+    low ? : number;
+    now ? : number;
+  }
+}
