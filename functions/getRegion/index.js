e7baac06d59243b6ee6f3f82d9a740c928e812da
yanceyleo <developer@yanceyleo.com>
2019-02-21 13:38:50 +0800

    init repo

---
+++ functions/getRegion/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const rp = require('request-promise')
+
+cloud.init()
+
+exports.main = async (event, context) => {
+  const region = event.region;
+  const res = await rp({
+    method: 'get',
+    uri: `https://www.yahoo.com/news/_tdnews/api/resource/WeatherSearch;text=${region}`,
+    json: true
+  }).then((body) => {
+    return {
+      regionList: body
+    }
+  }).catch(err => {
+    return err;
+  })
+  return res;
+}
